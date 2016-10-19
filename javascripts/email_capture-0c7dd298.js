36919502e3fbcee64436ac6f84203628f6875abf
Codeship CI <dev@makersacademy.com>
2016-10-19 14:31:03 +0000

    Site updated to 441705a
    --skip-ci

---
+++ javascripts/email_capture-0c7dd298.js
@@ -0,0 +1,68 @@
+/* Segment email capture takes in an email address and submits it to Segment.com
+ jQuery Plugin
+
+ Use:
+ ===
+ $('#form_id').emailCapture();
+
+ - Form should have one <input> element with attribute 'type=email'
+ - Form should have one label element with attribute 'for=email_input_id' (used to display error/success message)
+ - Email <input> can have segments added using a data-segments attributes with each value comma-seperated
+   e.g. <input type="email" data-segments="women,entrepeneurs">
+ */
+
+
+(function ($, analytics) {
+  'use strict';
+
+  $.fn.emailCapture = function () {
+
+    return $(this).each(function(index, element) {
+      var form = $(element);
+      var email = form.find('input[type=email]');
+      var label = form.find('label[for=' + email.attr('id') + ']');
+      var submitMsg = "Thank you for subscribing!";
+      var stage = "Signed up for newsletter";
+
+      form.on("submit", submitEmailToSegment);
+
+      function submitEmailToSegment(event) {
+        event.preventDefault();
+        console.log("Sending to Segment");
+        sendToSegment();
+        label.html(submitMsg).show(2000);
+      }
+
+      function sendToSegment() {
+        analytics.alias(email.val());
+        console.log(analytics);
+        analytics.identify(email.val(), analyticsProperties());
+        console.log(analytics);
+        analytics.track(stage);
+        console.log(analytics);
+      }
+
+      function analyticsProperties() {
+        var values = {
+          email: email.val(),
+          Newsletter: true
+        };
+
+        return addSegments(values);
+      }
+
+      function addSegments(values) {
+
+        var segments = email.data("segments");
+
+        if (segments) {
+          segments.split(",").forEach(function(element) {
+            values[element.trim()] = true;
+          });
+        }
+
+        return values;
+      }
+    });
+  };
+})(jQuery, analytics);
